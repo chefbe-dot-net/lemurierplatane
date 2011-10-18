cd2c4420dd6ff9144ae43ec66e4aac2fe0a55d3e
Bernard Lambeau <blambeau@gmail.com>
2011-10-18 20:26:06 +0200

    Attempt to let redeploy work

--- hooks/server/redeploy.rb
+++ hooks/server/redeploy.rb
@@ -3,7 +3,9 @@ require File.expand_path('../../commons', __FILE__)
 
 agent = WebSync::ServerAgent.new($root)
 agent.listen(:production_up_to_date) do |*args|
-  load(File.expand_path('../restart.rb', __FILE__))
+  IO.popen(File.expand_path('../restart.rb', __FILE__)) do |io|
+    puts io.readlines.join("\n")
+  end
 end
 begin
   puts "Nothing to be done." unless agent.synchronize
