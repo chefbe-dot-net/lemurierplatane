e15bf01b0e891171364a41686999c063dc496a18
Bernard Lambeau <blambeau@gmail.com>
2013-03-05 16:08:42 +0100

    Force bundle on open.rb error

--- scripts/open.rb
+++ scripts/open.rb
@@ -1,6 +1,18 @@
 #!/usr/bin/env ruby
 Encoding.default_external = Encoding::UTF_8
 Dir.chdir File.expand_path("../../", __FILE__)
-require 'bundler/setup'
+
+retried = false
+begin
+  require 'bundler'
+  Bundler.setup
+rescue
+  raise if retried
+  puts "Bundling..."
+  `bundle`
+  retried = true
+  retry
+end
+
 require 'polygon/script'
 Polygon::Script::Open.run(ARGV)
