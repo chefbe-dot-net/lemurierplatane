0c24d3705e312dbadfafc999c408a39c9f97fb47
Bernard Lambeau <blambeau@gmail.com>
2015-08-02 21:10:38 +0200

    Stop presenting website, activity is closed.

--- lib/app.rb
+++ lib/app.rb
@@ -4,48 +4,8 @@ require 'helpers'
 class App < Polygon::Base
   helpers Helpers
 
-  get '/sitemap.xml' do
-    content_type "application/xml"
-    wlang :sitemap, locals: sitemap_locals, layout: false
-  end
-
-  get "/" do
-    wlang :index, locals: locals('fr', nil)
-  end
-
-  get %r{^/(fr|nl|en)/?$} do |lang|
-    wlang :page, locals: locals(lang, '')
-  end
-
-  get %r{^/(fr|nl|en)/(.+)$} do |lang,path|
-    if locals = locals(lang, path)
-      wlang :page, locals: locals
-    else
-      not_found
-    end
-  end
-
-  def locals(lang, path)
-    base   = Path(settings.doc_folder)/lang
-
-    locals = case path
-    when NilClass
-      page_locals('')
-    when ->(s){ s.empty? }
-      page_locals(lang)
-    else
-      page_locals("#{lang}/#{path}")
-    end
-
-    if locals
-      locals.merge! menu: (base/'menu.md').read
-      locals.merge! last_update: last_update
-    end
-    locals
-  end
-
-  def last_update
-    ['tmp/restart.txt', 'Gemfile.lock'].map(&Path).find(&:exist?).mtime.strftime("%Y-%m-%d")
+  get "*" do
+    wlang :closed, locals: {}, layout: false
   end
 
 end
